dbed482df2d87070183c67705b0698c02ba97126
Neel Joshi <neelabhiramjoshi@gmail.com>
2021-03-30 00:58:44 +0530

    First commit

---
+++ component/Health.js
@@ -0,0 +1,61 @@
+import React, { Component,useEffect,useState } from 'react';
+import { ActivityIndicator, FlatList, Image, SafeAreaView,Linking } from 'react-native';
+import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body } from 'native-base';
+
+function Health(){
+  const country='in'
+  const category='health'
+  const key='66b9c12bd1064c5b91234b5a6c338ab2'
+  const [isLoading,setLoading]=useState(true)
+  const [data,setData]=useState([])
+
+  useEffect(()=>{
+    fetch(`https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${key}`)
+    .then((response)=>response.json())
+    .then((json)=>setData(json.articles))
+    .catch((error)=>console.error(error))
+    .finally(()=>setLoading(false))
+  },[])
+
+    return(
+      <SafeAreaView>
+        {isLoading?<ActivityIndicator/>:
+          <FlatList
+            data={data}
+            keyExtractor={({id},index)=>id}
+            renderItem={({item})=>(
+              <Container>
+              <Content>
+                <Card style={{flex: 0}}>
+                  <CardItem>
+                    <Left>
+                      <Thumbnail source={{uri: item.urlToImage}} />
+                      <Body>
+                        <Text>{item.title}</Text>
+                        <Text note>{item.publishedAt}</Text>
+                      </Body>
+                    </Left>
+                  </CardItem>
+                  <CardItem>
+                    <Body>
+                      <Image source={{uri: item.urlToImage}} style={{height: 200, width: 200, flex: 1}}/>
+                      <Text>
+                        {item.content}
+                      </Text>
+                      <Text style={{color: 'blue'}}
+                      onPress={() => Linking.openURL(item.url)}>
+                  Go to Page
+                </Text>
+                    </Body>
+                  </CardItem>
+                </Card>
+              </Content>
+            </Container>
+            )}
+          />
+        }
+      </SafeAreaView>
+    )
+}
+
+export default Health
---
+++ component/TabExample.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { Body, Container, Header, Tab, Tabs,ScrollableTab, Title,Left, Right} from 'native-base';
+import General from './General'
+import Business from './Business'
+import Entertainment from './Entertainment'
+import Health from './Health'
+import Sports from './Sports'
+import { StyleSheet, Text, View } from 'react-native';
+import {
+  useFonts,
+  Inter_900Black,
+} from '@expo-google-fonts/inter';
+
+function TabExample(){
+  return(
+    <Container>
+        <Header style={styles.container} hasTabs>
+        <Left/>
+        <Body>
+          <Title style={{
+            color:'#686d76'
+          }}>Today's Daily</Title>
+        </Body>
+      <Right/>
+        </Header>
+        <Tabs tabBarUnderlineStyle={{backgroundColor:'#ffefcf'}} renderTabBar={()=> <ScrollableTab />}>
+          <Tab heading="General" activeTabStyle={{backgroundColor:'#9fd8df'}} activeTextStyle={{color:'#ffefcf'}} textStyle={{color:'black'}}>
+            <General/>
+          </Tab>
+          <Tab heading="Business" activeTabStyle={{backgroundColor:'#9fd8df'}} textStyle={{color:'black'}} activeTextStyle={{color:'#ffefcf'}}>
+            <Business/>
+          </Tab>
+          <Tab heading="Entertainment" activeTabStyle={{backgroundColor:'#9fd8df'}} activeTextStyle={{color:'#ffefcf'}} textStyle={{color:'black'}}>
+            <Entertainment/>
+          </Tab>
+          <Tab heading="Health" activeTabStyle={{backgroundColor:'#9fd8df'}} activeTextStyle={{color:'#ffefcf'}} textStyle={{color:'black'}}>
+            <Health/>
+          </Tab>
+          <Tab heading="Sports" activeTabStyle={{backgroundColor:'#9fd8df'}} activeTextStyle={{color:'#ffefcf'}} textStyle={{color:'black'}}>
+            <Sports/>
+          </Tab>
+        </Tabs>
+      </Container>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    height: 140,
+    backgroundColor:'#dbf6e9',
+  },
+});
+
+
+
+export default TabExample
